8778a5a0f3b3b11ba075120836375a232734da14
Rei Ayanami <128055689+Rei-Ayanami-Buger-King@users.noreply.github.com>
2025-02-27 09:16:51 -0600

    Adding Variable stuff

--- tool.sh
+++ tool.sh
@@ -2,3 +2,6 @@
 #Testing git commits from visual studio code
 
 echo Hello World # This is a comment too!
+My_Message="Hello World2" #Assigns string to My_Message var
+echo $My_Message # Prints string from var using $ to reference it
+
