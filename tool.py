cfd4fc9b09760fb16099c4ae51219bbbb675f448
Rei Ayanami <128055689+Rei-Ayanami-Buger-King@users.noreply.github.com>
2025-02-28 04:00:28 -0600

    Actually added some content to the tool, addition and subtraction nothing special

---
+++ tool.py
@@ -0,0 +1,24 @@
+def main():
+    print("This is a simple addition and subtraction tool to practice cli interaction in python")
+    print("\n Would you like to add or subtract?")
+    choice = input("Enter choice:")
+    if choice == "addition":
+        addition()
+    else:
+        subtraction()
+
+def addition():
+    num1 = float(input("What would you like to add?"))
+    num2 = float(input("What second number would you like to add?"))
+    output = (num1+num2)
+    print(output)
+
+
+def subtraction():
+    num1 = float(input("What would you like to subtract?"))
+    num2 = float(input("What second number would you like to subtract?"))
+    output = (num1-num2)
+    print(output)
+
+
+main()
