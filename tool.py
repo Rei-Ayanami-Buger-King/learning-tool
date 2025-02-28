791a89aac97cbb2abc0392d26daef8bc2f240346
Rei Ayanami <128055689+Rei-Ayanami-Buger-King@users.noreply.github.com>
2025-02-28 04:02:40 -0600

    made it a loop

--- tool.py
+++ tool.py
@@ -1,3 +1,6 @@
+
+
+
 def main():
     print("This is a simple addition and subtraction tool to practice cli interaction in python")
     print("\n Would you like to add or subtract?")
@@ -13,12 +16,19 @@ def addition():
     output = (num1+num2)
     print(output)
 
+    main()
+
+
+
 
 def subtraction():
     num1 = float(input("What would you like to subtract?"))
     num2 = float(input("What second number would you like to subtract?"))
     output = (num1-num2)
     print(output)
+
+    main()
+
 
 
 main()
